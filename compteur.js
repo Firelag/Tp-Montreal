5c1db19edb7c98f4d3f339ccc305991b17e9d6f4
Firelag <jordan.vincent085@gmail.com>
2023-02-07 11:28:09 +0100

    commit 4

---
+++ compteur.js
@@ -0,0 +1,16 @@
+var n = 258; // Nombre final du compteur
+var cpt = 0; // Initialisation du compteur
+var duree = 5; // Durée en seconde pendant laquel le compteur ira de 0 à 15
+var delta = Math.ceil((duree * 1000) / n); // On calcule l'intervalle de temps entre chaque rafraîchissement du compteur (durée mise en milliseconde)
+var node =  document.getElementById("compteur"); // On récupère notre noeud où sera rafraîchi la valeur du compteur
+ 
+function countdown() {
+  node.innerHTML = ++cpt;
+  if( cpt < n ) { // Si on est pas arrivé à la valeur finale, on relance notre compteur une nouvelle fois
+     setTimeout(countdown, delta);
+  }
+}
+ 
+setTimeout(countdown, delta);
+
+
